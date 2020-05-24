97fbd99a262ea2d018a22a11bde561342a4a76ec
Avinash Bhingardive <avinash@freightbro.com>
2020-05-25 01:37:14 +0530

    new layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Todo from  './components/Todo';
 
 function App() {
   return (
-    <div></div>
+    <React.Fragment>
+      <main>
+        <div className="container">
+          <Todo />
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
 
